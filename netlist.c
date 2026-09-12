2882d58c5fe2b997cfa933010dfe4418a355a62e
Merlinhermes115 <dibbosyed@gmail.com>
2026-09-12 17:18:39 +0600

    added components header file

--- netlist.c
+++ netlist.c
@@ -1,17 +1,6 @@
 #include <stdio.h>
 
-#define MAX_POINTS 100
-#define MAX_COMPONENTS 100
-#define MAX_NAME 20
-
-typedef struct
-{
-    char name[MAX_NAME];
-    char type;      // R = resistor, V = voltage source, C = capacitor
-    int pointA;
-    int pointB;
-    double value;
-} Component;
+#include "components.h"
 
 
 // ---------------------------------------------------------
